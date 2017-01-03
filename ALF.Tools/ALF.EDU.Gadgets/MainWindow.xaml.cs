64a4dcbb0c5c64cd7b15e6b4f03a75cb11abc0b4
raulalf7 <raulalf7@163.com>
2017-01-03 16:39:50 +0800

    Added Daily Tools

---
+++ ALF.Tools/ALF.EDU.Gadgets/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ALF.EDU.Gadgets.UserControl;
+
+namespace ALF.EDU.Gadgets
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn == null)
+                return;
+            switch (btn.Content.ToString())
+            {
+                case "补充县基表实例和数据":
+                    var obj1 = new CountyInstanceSupply();
+                    mainContent.Content = obj1;
+                    mainContent.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+    }
+}
